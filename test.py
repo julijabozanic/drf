b992bed9dc90f5f5f35812de885da62fe37820fc
julijabozanic <julijabozanic@gmail.com>
2026-09-17 12:26:19 +0200

    Initial setup

---
+++ test.py
@@ -0,0 +1,21 @@
+a = [1, 2, 3]
+
+b = 1, 2, 3
+
+c = {"a": 1, "b": 2, "c": 3}
+print(type (a))
+print(type (b))
+print(type (c))
+
+def fun1(a, b):
+
+    return a + b
+
+print(fun1(1, 2))
+
+class MyClass:
+    def __init__(self, name):
+        self.name = names
+
+    def greet(self):
+        return f"Hello, {self.name}!"
